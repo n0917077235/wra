39fc59f763a36fc646add14e7135be595716461d
David Chen <davidchen9568@gmail.com>
2025-07-31 14:49:59 +0800

    Code formatting

--- main/backend/Wra10Core2023/SQLHelper.cs
+++ main/backend/Wra10Core2023/SQLHelper.cs
@@ -1,299 +1,298 @@
 ﻿using Microsoft.Data.SqlClient;
 using System.Data;
 
-namespace SQLHelper
+namespace SQLHelper;
+
+public class SQLHelper
 {
-    public class SQLHelper
+    private SqlConnection conn = null;
+    private SqlCommand cmd = null;
+    private SqlDataReader sdr = null;
+
+    public SQLHelper(string connStr)
     {
-        private SqlConnection conn = null;
-        private SqlCommand cmd = null;
-        private SqlDataReader sdr = null;
+        conn = new SqlConnection(connStr);
+    }
 
-        public SQLHelper(string connStr)
+    private SqlConnection GetConn()
+    {
+        if (conn.State == ConnectionState.Open)
         {
-            conn = new SqlConnection(connStr);
+            conn.Close();
         }
+        if (conn.State == ConnectionState.Closed)
+        {
+            conn.Open();
+        }
+        return conn;
+    }
 
-        private SqlConnection GetConn()
+    public int ExecuteNonQuery(string sql)
+    {
+        int res;
+        try
+        {
+            cmd = new SqlCommand(sql, GetConn());
+            cmd.CommandTimeout = 600;
+            res = cmd.ExecuteNonQuery();
+        }
+        catch (Exception ex)
+        {
+            res = -1;
+            throw ex;
+        }
+        finally
         {
             if (conn.State == ConnectionState.Open)
             {
                 conn.Close();
             }
-            if (conn.State == ConnectionState.Closed)
-            {
-                conn.Open();
-            }
-            return conn;
         }
+        return res;
+    }
 
-        public int ExecuteNonQuery(string sql)
+    public int ExecuteNonQuery(string sql, SqlParameter[] paras)
+    {
+        int res;
+        using (cmd = new SqlCommand(sql, GetConn()))
         {
-            int res;
+
+            cmd.CommandTimeout = 600;
+            cmd.Parameters.AddRange(paras);
             try
             {
-                cmd = new SqlCommand(sql, GetConn());
-                cmd.CommandTimeout = 600;
                 res = cmd.ExecuteNonQuery();
             }
             catch (Exception ex)
             {
-                res = -1;
-                throw ex;
-            }
-            finally
-            {
-                if (conn.State == ConnectionState.Open)
-                {
-                    conn.Close();
-                }
-            }
-            return res;
-        }
-
-        public int ExecuteNonQuery(string sql, SqlParameter[] paras)
-        {
-            int res;
-            using (cmd = new SqlCommand(sql, GetConn()))
-            {
-
-                cmd.CommandTimeout = 600;
-                cmd.Parameters.AddRange(paras);
-                try
-                {
-                    res = cmd.ExecuteNonQuery();
-                }
-                catch (Exception ex)
-                {
-                    cmd.Parameters.Clear();
-                    throw;
-                }
-
-            }
-            if (conn.State == ConnectionState.Open)
-            {
                 cmd.Parameters.Clear();
-                conn.Close();
+                throw;
             }
-            return res;
-        }
 
-        public int ExecuteScaler(string sql, SqlParameter[] paras)
+        }
+        if (conn.State == ConnectionState.Open)
         {
-            int res;
-            using (cmd = new SqlCommand(sql, GetConn()))
-            {
-
-                cmd.CommandTimeout = 600;
-                cmd.Parameters.AddRange(paras);
-                try
-                {
-                    res = (int)cmd.ExecuteScalar();
-
-                }
-                catch (Exception ex)
-                {
-                    cmd.Parameters.Clear();
-                    throw;
-                }
-
-            }
-            if (conn.State == ConnectionState.Open)
-            {
-                cmd.Parameters.Clear();
-                conn.Close();
-            }
-            return res;
+            cmd.Parameters.Clear();
+            conn.Close();
         }
+        return res;
+    }
 
-        public object ExecuteScalerObject(string sql, SqlParameter[] paras)
-        {
-            object res = null;
-            using (cmd = new SqlCommand(sql, GetConn()))
-            {
-
-                cmd.CommandTimeout = 600;
-                cmd.Parameters.AddRange(paras);
-                try
-                {
-                    res = cmd.ExecuteScalar();
-
-                }
-                catch (Exception ex)
-                {
-                    cmd.Parameters.Clear();
-                    throw;
-                }
-
-            }
-            if (conn.State == ConnectionState.Open)
-            {
-                cmd.Parameters.Clear();
-                conn.Close();
-            }
-            return res;
-        }
-
-        public DataTable ExecuteQuery(string sql)
-        {
-            DataTable dt = new DataTable();
-            cmd = new SqlCommand(sql, GetConn());
-            cmd.CommandTimeout = 600;
-            using (sdr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
-            {
-                dt.Load(sdr);
-            }
-            return dt;
-        }
-
-        public DataSet GetDataSet(string sql)
+    public int ExecuteScaler(string sql, SqlParameter[] paras)
+    {
+        int res;
+        using (cmd = new SqlCommand(sql, GetConn()))
         {
 
-            SqlDataAdapter sda = new SqlDataAdapter(sql, GetConn());
-            sda.SelectCommand.CommandTimeout = 600;
-            DataSet dset = new DataSet();
-            sda.Fill(dset);
-
-            return dset;
-        }
-
-        public DataTable ExecuteQuery(string sql, SqlParameter[] paras)
-        {
-            DataTable dt = new DataTable();
-            cmd = new SqlCommand(sql, GetConn());
-            cmd.CommandTimeout = 600;
-            cmd.Parameters.AddRange(paras);
-            using (sdr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
-            {
-                dt.Load(sdr);
-            }
-            return dt;
-        }
-
-        public DataTable ExecuteStoreProcedureQuery(string storeProcedure)
-        {
-            return ExecuteStoreProcedureQuery(storeProcedure, new SqlParameter[0]);
-        }
-
-        public DataTable ExecuteStoreProcedureQuery(string storeProcedure, SqlParameter[] paras)
-        {
-            DataTable dt = new DataTable();
-            cmd = new SqlCommand(storeProcedure, GetConn());
-            cmd.CommandType = CommandType.StoredProcedure;
-            cmd.CommandTimeout = 6000;
-            cmd.Parameters.AddRange(paras);
-            using (sdr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
-            {
-                dt.Load(sdr);
-            }
-            return dt;
-        }
-
-        public bool BoolExecuteQuery(string sql, SqlParameter[] paras)
-        {
-            DataTable dt = new DataTable();
-            cmd = new SqlCommand(sql, GetConn());
             cmd.CommandTimeout = 600;
             cmd.Parameters.AddRange(paras);
             try
             {
-                using (sdr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
-                {
-                    dt.Load(sdr);
-                }
+                res = (int)cmd.ExecuteScalar();
+
             }
-            catch (Exception e)
+            catch (Exception ex)
             {
-                throw e;
+                cmd.Parameters.Clear();
+                throw;
             }
-            DataRow[] rows = dt.Select();
-            bool temp = false;
-            if (rows.Length > 0)
-            {
-                temp = true;
-            }
-            return temp;
+
         }
-
-
-        public bool BoolExecuteQuery(string sql)
+        if (conn.State == ConnectionState.Open)
         {
-            DataTable dt = new DataTable();
-            cmd = new SqlCommand(sql, GetConn());
+            cmd.Parameters.Clear();
+            conn.Close();
+        }
+        return res;
+    }
+
+    public object ExecuteScalerObject(string sql, SqlParameter[] paras)
+    {
+        object res = null;
+        using (cmd = new SqlCommand(sql, GetConn()))
+        {
+
             cmd.CommandTimeout = 600;
+            cmd.Parameters.AddRange(paras);
+            try
+            {
+                res = cmd.ExecuteScalar();
+
+            }
+            catch (Exception ex)
+            {
+                cmd.Parameters.Clear();
+                throw;
+            }
+
+        }
+        if (conn.State == ConnectionState.Open)
+        {
+            cmd.Parameters.Clear();
+            conn.Close();
+        }
+        return res;
+    }
+
+    public DataTable ExecuteQuery(string sql)
+    {
+        DataTable dt = new DataTable();
+        cmd = new SqlCommand(sql, GetConn());
+        cmd.CommandTimeout = 600;
+        using (sdr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+        {
+            dt.Load(sdr);
+        }
+        return dt;
+    }
+
+    public DataSet GetDataSet(string sql)
+    {
+
+        SqlDataAdapter sda = new SqlDataAdapter(sql, GetConn());
+        sda.SelectCommand.CommandTimeout = 600;
+        DataSet dset = new DataSet();
+        sda.Fill(dset);
+
+        return dset;
+    }
+
+    public DataTable ExecuteQuery(string sql, SqlParameter[] paras)
+    {
+        DataTable dt = new DataTable();
+        cmd = new SqlCommand(sql, GetConn());
+        cmd.CommandTimeout = 600;
+        cmd.Parameters.AddRange(paras);
+        using (sdr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+        {
+            dt.Load(sdr);
+        }
+        return dt;
+    }
+
+    public DataTable ExecuteStoreProcedureQuery(string storeProcedure)
+    {
+        return ExecuteStoreProcedureQuery(storeProcedure, new SqlParameter[0]);
+    }
+
+    public DataTable ExecuteStoreProcedureQuery(string storeProcedure, SqlParameter[] paras)
+    {
+        DataTable dt = new DataTable();
+        cmd = new SqlCommand(storeProcedure, GetConn());
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.CommandTimeout = 6000;
+        cmd.Parameters.AddRange(paras);
+        using (sdr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+        {
+            dt.Load(sdr);
+        }
+        return dt;
+    }
+
+    public bool BoolExecuteQuery(string sql, SqlParameter[] paras)
+    {
+        DataTable dt = new DataTable();
+        cmd = new SqlCommand(sql, GetConn());
+        cmd.CommandTimeout = 600;
+        cmd.Parameters.AddRange(paras);
+        try
+        {
             using (sdr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
             {
                 dt.Load(sdr);
             }
-            DataRow[] rows = dt.Select();
-            bool temp = false;
-            if (rows.Length > 0)
-            {
-                temp = true;
-            }
-            return temp;
         }
-
-        /*
-        public static void WriteTextLog(string data)
+        catch (Exception e)
         {
-            string locationPath = EntryAssemblyLocation();
+            throw e;
+        }
+        DataRow[] rows = dt.Select();
+        bool temp = false;
+        if (rows.Length > 0)
+        {
+            temp = true;
+        }
+        return temp;
+    }
+
+
+    public bool BoolExecuteQuery(string sql)
+    {
+        DataTable dt = new DataTable();
+        cmd = new SqlCommand(sql, GetConn());
+        cmd.CommandTimeout = 600;
+        using (sdr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+        {
+            dt.Load(sdr);
+        }
+        DataRow[] rows = dt.Select();
+        bool temp = false;
+        if (rows.Length > 0)
+        {
+            temp = true;
+        }
+        return temp;
+    }
+
+    /*
+    public static void WriteTextLog(string data)
+    {
+        string locationPath = EntryAssemblyLocation();
+        try
+        {
+            if (!Directory.Exists(locationPath + @"\log"))
+            {
+                Directory.CreateDirectory(locationPath + @"\log");
+            }
+
+            lock (syncObject)
+            {
+                using (StreamWriter sw = new StreamWriter(
+                    locationPath + @"\log\" +
+                    DateTime.Now.ToString("yyyyMMdd") + @".log", true))
+                {
+                    sw.WriteLine(DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") + @"::" + data);
+                    sw.Close();
+                }
+            }
+        }
+        catch { }
+    }
+    */
+    public static object syncObject = new object();
+
+    public static void WriteDebug(string strMsg)
+    {
+        try
+        {
+            string path = System.Reflection.Assembly.GetEntryAssembly().Location.Replace(
+               System.Reflection.Assembly.GetEntryAssembly().ManifestModule.Name, "");
             try
             {
-                if (!Directory.Exists(locationPath + @"\log"))
+                if (!Directory.Exists(path + @"\Debug"))
                 {
-                    Directory.CreateDirectory(locationPath + @"\log");
+                    Directory.CreateDirectory(path + @"\Debug");
                 }
-
+                //Directory.CreateDirectory(path.Substring(0, nPos) + @"\Debug");
                 lock (syncObject)
                 {
                     using (StreamWriter sw = new StreamWriter(
-                        locationPath + @"\log\" +
-                        DateTime.Now.ToString("yyyyMMdd") + @".log", true))
+                        path + @"\Debug\" +
+                        DateTime.Now.ToString("yyyyMMdd") + @"_DebugM.txt", true))
                     {
-                        sw.WriteLine(DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") + @"::" + data);
+                        sw.WriteLine(DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") + @"::" + strMsg);
                         sw.Close();
                     }
                 }
             }
-            catch { }
+            catch
+            {
+            }
         }
-        */
-        public static object syncObject = new object();
 
-        public static void WriteDebug(string strMsg)
+        catch (Exception e)
         {
-            try
-            {
-                string path = System.Reflection.Assembly.GetEntryAssembly().Location.Replace(
-                   System.Reflection.Assembly.GetEntryAssembly().ManifestModule.Name, "");
-                try
-                {
-                    if (!Directory.Exists(path + @"\Debug"))
-                    {
-                        Directory.CreateDirectory(path + @"\Debug");
-                    }
-                    //Directory.CreateDirectory(path.Substring(0, nPos) + @"\Debug");
-                    lock (syncObject)
-                    {
-                        using (StreamWriter sw = new StreamWriter(
-                            path + @"\Debug\" +
-                            DateTime.Now.ToString("yyyyMMdd") + @"_DebugM.txt", true))
-                        {
-                            sw.WriteLine(DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss") + @"::" + strMsg);
-                            sw.Close();
-                        }
-                    }
-                }
-                catch
-                {
-                }
-            }
-
-            catch (Exception e)
-            {
-            }
         }
-
     }
+
 }
